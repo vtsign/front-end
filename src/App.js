44ac134eabcaa996df6e9b1c310a55a9c5885d6e
Mai Văn Tuấn <40934221+mvtuan@users.noreply.github.com>
2021-09-20 22:52:59 +0700

    ♻️ Remove unnecessary files and format code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<BrowserRouter>
+			<Header />
+			<main>
+				<Route path="/login" component={Login} />
+				<Route path="/register" component={Register} />
+			</main>
+			<Footer />
+		</BrowserRouter>
+	);
+};
 
 export default App;
