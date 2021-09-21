ffdbec021115b6a7841a3a69a0be1609b04636f9
Mai Văn Tuấn <40934221+mvtuan@users.noreply.github.com>
2021-09-21 22:43:37 +0700

    💄 Add Footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.scss';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
