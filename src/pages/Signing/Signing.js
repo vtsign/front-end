bc4971fc7ae8be242f0297cfba2e52688dad8a4b
Mai Văn Tuấn <40934221+mvtuan@users.noreply.github.com>
2021-10-20 19:43:05 +0700

    ✨ Drag and drop fields

--- src/pages/Signing/Signing.js
+++ src/pages/Signing/Signing.js
@@ -15,8 +15,10 @@ import {
 	CardContent,
 	InputLabel,
 	TextField,
+	Stack,
+	IconButton
 } from '@mui/material';
-import { CloudUpload, InsertDriveFile } from '@mui/icons-material';
+import { CloudUpload, InsertDriveFile, BorderColor, CalendarToday, TextFields } from '@mui/icons-material';
 import { useForm } from 'react-hook-form';
 import Header from '../../components/Header/Header';
 import WebViewer from '@pdftron/webviewer';
@@ -390,14 +392,9 @@ export function SecondStep({ receivers, setReceivers }) {
 }
 
 export function ThirdStep({ viewer, fileData, receivers }) {
-	const dragOver = (e) => {
-		e.preventDefault();
-		return false;
-	};
-	const drop = (e, instance) => {
-		e.preventDefault();
-		return false;
-	};
+	const [instance, setInstance] = useState(null);
+	const [dropPoint, setDropPoint] = useState(null);
+
 	useEffect(() => {
 		WebViewer(
 			{
@@ -407,6 +404,7 @@ export function ThirdStep({ viewer, fileData, receivers }) {
 		).then((instance) => {
 			// setInstanced(instance);
 			const { docViewer, iframeWindow, Annotations } = instance;
+			setInstance(instance);
 			docViewer.loadDocument(fileData.data);
 			Annotations.SignatureWidgetAnnotation.prototype.createSignHereElement = function () {
 				const div = document.createElement('div');
@@ -428,6 +426,92 @@ export function ThirdStep({ viewer, fileData, receivers }) {
 			});
 		});
 	}, []);
+
+	const addField = (type, point = {}, name = '', value = '', flag = {}) => {
+		const { docViewer, Annotations } = instance;
+		const annotManager = docViewer.getAnnotationManager();
+		const doc = docViewer.getDocument();
+		const displayMode = docViewer.getDisplayModeManager().getDisplayMode();
+		const page = displayMode.getSelectedPages(point, point);
+		if (!!point.x && page.first == null) {
+			return; //don't add field to an invalid page location
+		}
+		const page_idx = page.first !== null ? page.first : docViewer.getCurrentPage();
+		const page_info = doc.getPageInfo(page_idx);
+		const page_point = displayMode.windowToPage(point, page_idx);
+		const zoom = docViewer.getZoom();
+
+		var textAnnot = new Annotations.FreeTextAnnotation();
+		textAnnot.PageNumber = page_idx;
+		const rotation = docViewer.getCompleteRotation(page_idx) * 90;
+		textAnnot.Rotation = rotation;
+		if (rotation === 270 || rotation === 90) {
+			textAnnot.Width = 50.0 / zoom;
+			textAnnot.Height = 250.0 / zoom;
+		} else {
+			textAnnot.Width = 250.0 / zoom;
+			textAnnot.Height = 50.0 / zoom;
+		}
+		textAnnot.X = (page_point.x || page_info.width / 2) - textAnnot.Width / 2;
+		textAnnot.Y = (page_point.y || page_info.height / 2) - textAnnot.Height / 2;
+
+		textAnnot.setPadding(new Annotations.Rect(0, 0, 0, 0));
+		textAnnot.custom = {
+			type,
+			value,
+			flag,
+			name: `${type}_`,
+		};
+
+		// set the type of annot
+		textAnnot.setContents(textAnnot.custom.name);
+		textAnnot.FontSize = '' + 20.0 / zoom + 'px';
+		textAnnot.FillColor = new Annotations.Color(211, 211, 211, 0.5);
+		textAnnot.TextColor = new Annotations.Color(0, 165, 228);
+		textAnnot.StrokeThickness = 1;
+		textAnnot.StrokeColor = new Annotations.Color(0, 165, 228);
+		textAnnot.TextAlign = 'center';
+
+		textAnnot.Author = annotManager.getCurrentUser();
+
+		annotManager.deselectAllAnnotations();
+		annotManager.addAnnotation(textAnnot, true);
+		annotManager.redrawAnnotation(textAnnot);
+		annotManager.selectAnnotation(textAnnot);
+	};
+
+	const dragOver = (e) => {
+		e.preventDefault();
+		return false;
+	};
+
+	const drop = (e, instance) => {
+		const { docViewer } = instance;
+		const scrollElement = docViewer.getScrollViewElement();
+		const scrollLeft = scrollElement.scrollLeft || 0;
+		const scrollTop = scrollElement.scrollTop || 0;
+		setDropPoint({ x: e.pageX + scrollLeft, y: e.pageY + scrollTop });
+		e.preventDefault();
+		return false;
+	};
+
+	const dragStart = (e) => {
+		e.target.style.opacity = 0.5;
+		const copy = e.target.cloneNode(true);
+		copy.id = 'form-build-drag-image-copy';
+		copy.style.width = '250px';
+		document.body.appendChild(copy);
+		e.dataTransfer.setDragImage(copy, 125, 25);
+		e.dataTransfer.setData('text', '');
+	};
+
+	const dragEnd = (e, type) => {
+		addField(type, dropPoint);
+		e.target.style.opacity = 1;
+		document.body.removeChild(document.getElementById('form-build-drag-image-copy'));
+		e.preventDefault();
+	};
+
 	return (
 		<>
 			<Grid>
@@ -436,6 +520,74 @@ export function ThirdStep({ viewer, fileData, receivers }) {
 				</Typography>
 			</Grid>
 			<Grid display="flex" my="1rem">
+				<Stack>
+					{/* <Box padding={2}>
+						<Text>{'Step 2'}</Text>
+					</Box>
+					<Box padding={2}>
+						<SelectList
+							id="assigningFor"
+							name="assign"
+							onChange={({ value }) => setAssignee(value)}
+							options={assigneesValues}
+							placeholder="Select recipient"
+							label="Adding signature for"
+							value={assignee}
+						/>
+					</Box> */}
+					<Box padding={2}>
+						<div
+							draggable
+							onDragStart={(e) => dragStart(e)}
+							onDragEnd={(e) => dragEnd(e, 'SIGNATURE')}
+						>
+							<IconButton
+								style={{ color: 'black' }}
+								onClick={() => addField('SIGNATURE')}
+								accessibilityLabel="add signature"
+								text="Add signature"
+								iconEnd="compose"
+							>
+								<BorderColor fontSize="inherit" />
+							</IconButton>
+							<span>Signature</span>
+						</div>
+					</Box>
+					<Box padding={2}>
+						<div
+							draggable
+							onDragStart={(e) => dragStart(e)}
+							onDragEnd={(e) => dragEnd(e, 'TEXT')}
+						>
+							<IconButton
+								onClick={() => addField('TEXT')}
+								accessibilityLabel="add text"
+								text="Add text"
+								iconEnd="text-sentence-case"
+							>
+								<TextFields fontSize="inherit" />
+							</IconButton>
+							<span>Text</span>
+						</div>
+					</Box>
+					<Box padding={2}>
+						<div
+							draggable
+							onDragStart={(e) => dragStart(e)}
+							onDragEnd={(e) => dragEnd(e, 'DATE')}
+						>
+							<IconButton
+								onClick={() => addField('DATE')}
+								accessibilityLabel="add date field"
+								text="Add date"
+								iconEnd="calendar"
+							>
+								<CalendarToday fontSize="inherit" />
+							</IconButton>
+							<span>Date</span>
+						</div>
+					</Box>
+				</Stack>
 				<div style={{ width: '1000px', height: '500px' }} ref={viewer} />
 				{/* <Grid item lg={8} md={12} xl={9} xs={12} mr="2rem">
 					<Card>
@@ -752,7 +904,11 @@ const Signing = () => {
 							<SecondStep receivers={receivers} setReceivers={setReceivers} />
 						)}
 						{activeStep === 2 && (
-							<ThirdStep viewer={viewer} fileData={fileData} receivers={receivers} />
+							<ThirdStep
+								viewer={viewer}
+								fileData={fileData}
+								receivers={receivers}
+							/>
 						)}
 						{activeStep === 3 && <LastStep />}
 						<Grid display="flex" justifyContent="flex-end">
